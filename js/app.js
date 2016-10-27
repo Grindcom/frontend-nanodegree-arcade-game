09c251023058c0b4fd4b2e484e39abd132b56af7
Greg Ford <ford@grindcom.ca>
2016-10-27 10:52:31 -0700

    Semi-colon on all function declarations.

--- js/app.js
+++ js/app.js
@@ -70,7 +70,7 @@ var PLAYER_START_Y = 450;
 function randPos(){
     var rnd = Math.floor(Math.random() * 100);
     return rnd + 100;
-}
+};
 /*************************************************
 //
 //      LANE CLASS
@@ -427,9 +427,7 @@ function setScore(newScore){
     // set the fill text and location
     ctx.fillText(newScore,canvas.width - 20, 35);
 };
-function redrawScore(score){
 
-};
 //
 // Show LIVES remaining
 //
@@ -542,10 +540,6 @@ function checkCollisions(){
 //       STARTUP AND INIT FUNCTIONS
 //
 ***************************************************************/
-function initLives(){
-    // console.log("Init Lives");
-
-};
 //
 // Function to set up Meme text style
 //
@@ -593,4 +587,4 @@ function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
