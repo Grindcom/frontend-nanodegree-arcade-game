c6661f1199cef7befa8416cb43ca0faee67742ee
Greg Ford <ford@grindcom.ca>
2016-10-28 14:25:15 -0700

    Beutify the app.js file.

--- js/app.js
+++ js/app.js
@@ -37,7 +37,7 @@ var RIGHTWALL = 600;
 // Column center X values
 //  These values correspond to the player x value
 //  that puts the player icon in the center of a column
-var COLUMN_CENTERS = [0,100,200,300,400];
+var COLUMN_CENTERS = [0, 100, 200, 300, 400];
 //
 // Number of enemies on current level
 //
@@ -67,7 +67,7 @@ var PLAYER_START_Y = 450;
 //
 // Random postion generator
 //
-function randPos(){
+function randPos() {
     var rnd = Math.floor(Math.random() * 100);
     return rnd + 100;
 }
@@ -76,11 +76,11 @@ function randPos(){
 //      LANE CLASS
 //
 ***************************************************/
-var Lane = function(t,b){
+var Lane = function(t, b) {
     this.id = 0;
     this.topY = t;
     this.bottomY = b;
-    this.track = ((b+t)/2) + 12;// set to middle of lane
+    this.track = ((b + t) / 2) + 12; // set to middle of lane
     this.safetyZone = "unknown";
 
 };
@@ -90,22 +90,22 @@ var Lane = function(t,b){
 //      REWARD CLASS
 //
 ***************************************************/
-var Gem = function(){
+var Gem = function() {
     this.sprite = 'images/Rock.png';
     this.x = 0;
     this.y = 0;
-    this.currentLane = null;// Lane to place gem into
-    this.upScore = 0;// Amount to multiply player score by.
+    this.currentLane = null; // Lane to place gem into
+    this.upScore = 0; // Amount to multiply player score by.
     this.hide = false;
     this.lastTime = 0;
 };
 //
-Gem.prototype.update = function(dt){
-    var tmp = dt*1000;
-    if(this.lastTime > 0){
+Gem.prototype.update = function(dt) {
+    var tmp = dt * 1000;
+    if (this.lastTime > 0) {
         var timeInterval = 20;
         // if hidden, show again after a time interval
-        if(this.hide && (tmp - this.lastTime) > timeInterval){
+        if (this.hide && (tmp - this.lastTime) > timeInterval) {
             // set a random location
             this.randLoc();
             // Make visible.
@@ -113,23 +113,23 @@ Gem.prototype.update = function(dt){
             // Record the time to compare later.
             this.lastTime = dt;
         }
-    }else{
+    } else {
         this.lastTime = tmp;
     }
 };
 //
 Gem.prototype.render = function() {
-    if(!this.hide){
+    if (!this.hide) {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 };
 //
-Gem.prototype.randLoc = function(){
+Gem.prototype.randLoc = function() {
     // Set to a random Lane
-    this.currentLane = getRandLane(0,gameLanes.length - 1);
+    this.currentLane = getRandLane(0, gameLanes.length - 1);
     this.y = gameLanes[this.currentLane].track;
     // set to a random column
-    var tmp = getRandLane(0,COLUMN_CENTERS.length -1);
+    var tmp = getRandLane(0, COLUMN_CENTERS.length - 1);
     this.x = COLUMN_CENTERS[tmp];
 };
 /**************************************************
@@ -159,15 +159,15 @@ Enemy.prototype.update = function(dt) {
     // console.log("Enemy Update: " + dt);
     //
     // Check to see if enemy has hit the wall
-    if(this.x >= RIGHTWALL){
+    if (this.x >= RIGHTWALL) {
         // TODO:  Set up a random delay before starting again
         // Move back to start
         this.x = 1 - Math.floor(Math.random() * 100);
         // console.log(this.x);
         // Increase the speed
-        enemySpeed = (enemySpeed+1) * dt;
+        enemySpeed = (enemySpeed + 1) * dt;
         // Change lane at random
-        this.setLane(getRandLane(ENEMY_LANE_MIN,ENEMY_LANE_MAX));
+        this.setLane(getRandLane(ENEMY_LANE_MIN, ENEMY_LANE_MAX));
     } else {
         this.x = ++this.x;
     }
@@ -181,9 +181,9 @@ Enemy.prototype.render = function() {
 };
 
 // Change lanes
-Enemy.prototype.setLane = function(lane){
+Enemy.prototype.setLane = function(lane) {
     // if lane is in range
-    if(lane <= ENEMY_LANE_MAX && lane >= ENEMY_LANE_MIN){
+    if (lane <= ENEMY_LANE_MAX && lane >= ENEMY_LANE_MIN) {
         // set new currentLane
         this.currentLane = lane;
     }
@@ -198,7 +198,7 @@ Enemy.prototype.setLane = function(lane){
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(){
+var Player = function() {
     // Initilize icon for player
     this.sprite = 'images/char-boy-2.png';
     // Initial x location for player
@@ -209,21 +209,21 @@ var Player = function(){
     this.currentLane = 5;
 };
 //
-Player.prototype.update = function(dt){
+Player.prototype.update = function(dt) {
     checkCollisions();
 };
 //
-Player.prototype.render = function(){
+Player.prototype.render = function() {
     // If there are lives left, render the player icon.
-    if(lives > 0){
+    if (lives > 0) {
         //
         // If player is in the score lane
         //  move towards the score box
-        if(this.currentLane === 0){
-            if(this.x >= RIGHTWALL){
+        if (this.currentLane === 0) {
+            if (this.x >= RIGHTWALL) {
                 // Go back to start
                 this.startPosition();
-            }else{
+            } else {
                 // keep moving towards score box
                 this.x++;
             }
@@ -233,7 +233,7 @@ Player.prototype.render = function(){
     }
 };
 // Re-locate Player to default position
-Player.prototype.startPosition = function(){
+Player.prototype.startPosition = function() {
     this.x = PLAYER_START_X;
     this.y = PLAYER_START_Y;
     this.currentLane = 5;
@@ -242,68 +242,68 @@ Player.prototype.startPosition = function(){
 };
 // Interpret user input
 var ignoreInput = Boolean(false);
-Player.prototype.handleInput = function(key){
+Player.prototype.handleInput = function(key) {
     var advanceFactor = 10;
 
     switch (key) {
         case 'left':
-        // If not all the way left
-        if(this.x > 0){
-            // decrement x
-            this.x -= advanceFactor;
-        }
-        break;
+            // If not all the way left
+            if (this.x > 0) {
+                // decrement x
+                this.x -= advanceFactor;
+            }
+            break;
         case 'up':
-        // If not all the way up
-        if(this.y > 50){
-            // decrement y
-            this.y -= advanceFactor;
-        }
-        // If the current lane becomes zero, the Score increases
-        if(this.currentLane == 0){
-            // Increase the score
-            gameScore++;
-            // Send score to canvas
-            setScore(gameScore);
-            // Ignore input (until player.startPosition is called)
-            ignoreInput = true;
-            // Otherwise, Compare the current position with the next lanes tracke
-        }else if(this.y < gameLanes[this.currentLane-1].track){
-            // If player is past the next lanes center track move to the next lane.
-            this.currentLane--;
-        }
-        break;
+            // If not all the way up
+            if (this.y > 50) {
+                // decrement y
+                this.y -= advanceFactor;
+            }
+            // If the current lane becomes zero, the Score increases
+            if (this.currentLane == 0) {
+                // Increase the score
+                gameScore++;
+                // Send score to canvas
+                setScore(gameScore);
+                // Ignore input (until player.startPosition is called)
+                ignoreInput = true;
+                // Otherwise, Compare the current position with the next lanes tracke
+            } else if (this.y < gameLanes[this.currentLane - 1].track) {
+                // If player is past the next lanes center track move to the next lane.
+                this.currentLane--;
+            }
+            break;
         case 'right':
-        // If not all the way right
-        if(this.x < (RIGHTWALL - 186)){
-            // Increment x
-            this.x += advanceFactor;
-        }
-        break;
+            // If not all the way right
+            if (this.x < (RIGHTWALL - 186)) {
+                // Increment x
+                this.x += advanceFactor;
+            }
+            break;
         case 'down':
-        // If not at bottom
-        if(this.y < BOTTOMWALL){
-            // Increment y
-            this.y += advanceFactor;
-        }else{// Otherwise assume player is at the bottom and bounce the icon back.
-            // bounce back
-            this.y = (BOTTOMWALL - 100);
-            // TODO:  call for a flip of icon
-        }
-        // Make sure the current lane is within the gameLanes array size
-        if(this.currentLane >= gameLanes.length){
-            this.currentLane--;
-            // Check for lane change
-        }else if(this.y > gameLanes[this.currentLane].track){
-            // If player is past the next lanes center track move to the next lane.
-            this.currentLane++;
-        }
-        break;
+            // If not at bottom
+            if (this.y < BOTTOMWALL) {
+                // Increment y
+                this.y += advanceFactor;
+            } else { // Otherwise assume player is at the bottom and bounce the icon back.
+                // bounce back
+                this.y = (BOTTOMWALL - 100);
+                // TODO:  call for a flip of icon
+            }
+            // Make sure the current lane is within the gameLanes array size
+            if (this.currentLane >= gameLanes.length) {
+                this.currentLane--;
+                // Check for lane change
+            } else if (this.y > gameLanes[this.currentLane].track) {
+                // If player is past the next lanes center track move to the next lane.
+                this.currentLane++;
+            }
+            break;
         case 'jump':
-        this.y -= 100;
-        break;
+            this.y -= 100;
+            break;
         default:
-        console.log("Not valid input");
+            console.log("Not valid input");
     }
 };
 
@@ -313,7 +313,8 @@ Player.prototype.handleInput = function(key){
 //
 ***************************************************/
 var gems = [];
-function initGems(){
+
+function initGems() {
     // set up Blue Gem
     gems[0] = new Gem();
     gems[0].sprite = 'images/Gem Blue-2.png';
@@ -333,32 +334,33 @@ function initGems(){
     gems[3] = new Gem();
     gems[3].sprite = 'images/Rock-2.png';
     gems[3].randLoc();
-    gems[3].upScore = 0;//
+    gems[3].upScore = 0; //
 
 }
 
 
 // Now instantiate your objects.
 var gameLanes = [];
-function initLanes(){
+
+function initLanes() {
     var i = 0;
     // initialize score lane
-    gameLanes[i] = new Lane((i*LANEHEIGHT),(i*LANEHEIGHT)+LANEHEIGHT);
+    gameLanes[i] = new Lane((i * LANEHEIGHT), (i * LANEHEIGHT) + LANEHEIGHT);
     gameLanes[i].safetyZone = "score";
     gameLanes[i].id = i;
     //
     // Set up enemy lanes
     //
-    for(i = 1; i <= 3/*number of danger lanes*/;i++){
-        gameLanes[i] = new Lane((i*LANEHEIGHT),(i*LANEHEIGHT)+LANEHEIGHT);
+    for (i = 1; i <= 3 /*number of danger lanes*/ ; i++) {
+        gameLanes[i] = new Lane((i * LANEHEIGHT), (i * LANEHEIGHT) + LANEHEIGHT);
         gameLanes[i].safetyZone = "danger";
         gameLanes[i].id = i;
     }
     //
     // Set up the safe lanes
     //
-    for(; i < totalLanes; i++){
-        gameLanes[i] = new Lane((i*LANEHEIGHT),(i*LANEHEIGHT)+LANEHEIGHT);
+    for (; i < totalLanes; i++) {
+        gameLanes[i] = new Lane((i * LANEHEIGHT), (i * LANEHEIGHT) + LANEHEIGHT);
         gameLanes[i].safetyZone = "safe";
         gameLanes[i].id = i;
     }
@@ -367,14 +369,15 @@ function initLanes(){
 // Place all enemy objects in an array called allEnemies
 //
 var allEnemies = [];
-function initEnemies(){
-    for(var i = 0; i < ENEMY_TOTAL /*number of enemies*/; i++){
+
+function initEnemies() {
+    for (var i = 0; i < ENEMY_TOTAL /*number of enemies*/ ; i++) {
         // Select one of the danger lanes at random
-        var selectedLane = getRandomIntInclusive(ENEMY_LANE_MIN,ENEMY_LANE_MAX);
+        var selectedLane = getRandomIntInclusive(ENEMY_LANE_MIN, ENEMY_LANE_MAX);
         // Make a new enemy object
         allEnemies[i] = new Enemy();
         // Place the enemy in a lane
-        allEnemies[i].y = gameLanes[selectedLane].track;//i*90 + 50;
+        allEnemies[i].y = gameLanes[selectedLane].track; //i*90 + 50;
         // Randomly place the enemy on the x axis
         allEnemies[i].x = (Math.floor(Math.random() * 100) + 100) * i;
         // Store the lane this enemy will be in
@@ -402,7 +405,7 @@ document.addEventListener('keyup', function(e) {
     // Use this to find any key code
     // console.log("Key Listener: " + e.keyCode);
     // Call for an action
-    if(!ignoreInput){
+    if (!ignoreInput) {
         player.handleInput(allowedKeys[e.keyCode]);
     }
 
@@ -416,23 +419,23 @@ document.addEventListener('keyup', function(e) {
 //
 // Set the score
 //
-function setScore(newScore){
+function setScore(newScore) {
     // Clear canvas so score text is clean
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.textAlign = "right";
     // Set the outside text and location
-    ctx.strokeText(newScore,canvas.width - 20, 35);
+    ctx.strokeText(newScore, canvas.width - 20, 35);
     // set the fill text and location
-    ctx.fillText(newScore,canvas.width - 20, 35);
+    ctx.fillText(newScore, canvas.width - 20, 35);
 }
 
 //
 // Show lives remaining
 //
-function showLife(){
+function showLife() {
     var i;
-    for(i = 0; i < lives; i++){
-        ctx.drawImage(Resources.get('images/Heart-2-tiny.png'), i*25, 0);
+    for (i = 0; i < lives; i++) {
+        ctx.drawImage(Resources.get('images/Heart-2-tiny.png'), i * 25, 0);
     }
     //
 }
@@ -447,39 +450,39 @@ function showLife(){
 // Pick a random lane between the given parameters
 //  ( expect low is less than high)
 //
-function getRandLane(low,high){
-    var maxLanes = (gameLanes.length)-1;
-    var newLane = getRandomIntInclusive(low,high);
+function getRandLane(low, high) {
+    var maxLanes = (gameLanes.length) - 1;
+    var newLane = getRandomIntInclusive(low, high);
     return newLane;
 }
 //
 // Check for player v. obstacle collisions
 //
-function checkCollisions(){
+function checkCollisions() {
     // Make sure test is within array bounds
     //  if current lane is same as game lane size
-    if( player.currentLane >= gameLanes.length ){
+    if (player.currentLane >= gameLanes.length) {
         // Do nothing
         return;
     }
     // Check wheter player is in a danger Zone
-    if(gameLanes[player.currentLane].safetyZone == "danger"){
+    if (gameLanes[player.currentLane].safetyZone == "danger") {
         // Compare player position with enemies that are in the same lane
-        allEnemies.forEach(function(enemy){
+        allEnemies.forEach(function(enemy) {
             // If the enemy and player are in the same lane
-            if(enemy.currentLane == player.currentLane){
+            if (enemy.currentLane == player.currentLane) {
                 // set a range where player is killed
                 var low = enemy.x - 75;
                 var high = enemy.x + 75;
                 // compare the players x position with the kill zone
-                if(player.x > low && player.x < high){
+                if (player.x > low && player.x < high) {
                     // If in the kill zone send back to start
                     player.startPosition();
                     lives--;
-                    if(!lives){
+                    if (!lives) {
                         // Send game over message with score
                         setScore("Game Over!  Your Score: " + gameScore)
-                        //
+                            //
                         return;
                     }
                     // Clear canvas so score text is clean
@@ -495,18 +498,18 @@ function checkCollisions(){
     // Check for collisions with gems
     //  Bugs make gems hide and a player
     //  collision affects score.
-    gems.forEach(function(gem){
+    gems.forEach(function(gem) {
         var low = gem.x - 75;
         var high = gem.x + 75;
         // If the gem isn't hidden see if
         //  it is in a collision zone.
-        if(!gem.hide){
+        if (!gem.hide) {
             // Check against player first
             //  Are they in the same lane?
-            if(gem.currentLane == player.currentLane){
-                if(player.x > low && player.x < high){
+            if (gem.currentLane == player.currentLane) {
+                if (player.x > low && player.x < high) {
                     // Make sure score can increase if possible
-                    if(gameScore <= 0){
+                    if (gameScore <= 0) {
                         gameScore = 1;
                     }
                     // multiply current score by the gems bonus amount
@@ -518,11 +521,11 @@ function checkCollisions(){
             }
             // Check against all Bugs
             //
-            allEnemies.forEach(function(enemy){
+            allEnemies.forEach(function(enemy) {
                 // Are they in the same lane?
-                if(gem.currentLane == enemy.currentLane){
+                if (gem.currentLane == enemy.currentLane) {
                     // if they are in same square
-                    if(enemy.x > low && enemy.x < high){
+                    if (enemy.x > low && enemy.x < high) {
                         // Hide the gem.
                         gem.hide = true;
                     }
@@ -542,14 +545,14 @@ function checkCollisions(){
 //
 // Function to set up Meme text style
 //
-function initText (context){
+function initText(context) {
     // Set font size and type
     context.font = "36px Impact";
     context.textAlign = 'center';
     // Set outline color
-    context.strokeStyle = "black";// this is the default color
+    context.strokeStyle = "black"; // this is the default color
     // Set fill color
-    context.fillStyle = "white";// this is the default color
+    context.fillStyle = "white"; // this is the default color
     // Set the stroke width
     context.lineWidth = 3;
     // Show initial score
@@ -559,7 +562,7 @@ function initText (context){
 // When this page is loaded initialize the required variables
 //  and arrays
 //
-window.onload = function(){
+window.onload = function() {
     //
     // Initialize the score text
     initText(ctx);
